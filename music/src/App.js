963d310f12b960ea94278013ba673a4f1e261ef9
code1511 <anch20001511@gmail.com>
2021-11-21 20:20:14 +0530

    update in app.js

--- music/src/App.js
+++ music/src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Side hustle</h1>
+     <h1>log in </h1>
+     <form>
+       <input className="input" type="text" placeholder="Username"></input>
+       <br/>
+       <input className="input" type="text" placeholder="Password"></input>
+       <br/>
+       <input className="input" type="submit" value="Submit"></input>
+       <br/>
+       <lable>Not registered yet, Register now !</lable>
+       <br/>
+       <input className="input" type="submit" value="Register"></input>
+
+     </form>
     </div>
   );
 }
