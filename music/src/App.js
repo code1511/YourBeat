1bc16c9bf8a51a2a4c3f5b993ba74a4b0c5fb8f4
code1511 <anch20001511@gmail.com>
2021-11-21 22:19:31 +0530

    update in app.js

--- music/src/App.js
+++ music/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
+// import "./img/bgImg1"
 
 function App() {
   return (
     <div className="App">
      <form className="box-form">
      <h1 className="top">log in </h1>
-       <input className="input" type="text" placeholder="Username"></input>
+       <input className="input"  type="text" name="username" placeholder="Username" required></input>
        <br/>
-       <input className="input" type="text" placeholder="Password"></input>
+       <input className="input" type="password" name="password" placeholder="Password" required></input>
        <br/>
        <br/>
-       <input className="input" type="submit" value="Submit"></input>
+       <input className="Sub-button" type="submit" value="Submit" ></input>
        <br/>
        <lable>Not registered yet, Register now !</lable>
        <br/>
-       <input className="input" type="submit" value="Register"></input>
+       <input className="Sub-button" type="submit" value="Register"></input>
 
      </form>
     </div>
