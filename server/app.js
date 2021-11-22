8d2f5294888b85caf60c9c174f732c118e4da29f
code1511 <anch20001511@gmail.com>
2021-11-22 18:46:12 +0530

    done

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log("anchal");
+const http =require("http");
+
+http.createServer((req,res)=>{
+
+  res.writeHead(200,{'Content-type':'text/html'});
+  res.end('<h1>Hello world</h1>');
+
+}).listen(3000,()=>{
+    console.log("server running");
+})
