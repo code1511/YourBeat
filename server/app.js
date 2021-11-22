4ed28622cb131b3c46ebcf7ca10ed557749ca726
code1511 <anch20001511@gmail.com>
2021-11-22 13:32:13 +0530

    update

--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('launching page')
+  })
+
+app.get('/user', (req, res) => {
+  res.send('Got a GET request at /user')
 })
 
 app.listen(port, () => {
