7568a170ba2d8d1e33d1a816ef266db292422102
code1511 <anch20001511@gmail.com>
2021-11-22 13:55:41 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,16 +1,1 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-
-app.get('/', (req, res) => {
-    res.send('launching page')
-  })
-
-app.get('/user', (req, res) => {
-  res.send('Got a GET request at /user')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+console.log("anchal");
