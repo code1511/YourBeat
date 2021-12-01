03f018122f8a7e364aef5240456e8d1fe834dbcd
code1511 <anch20001511@gmail.com>
2021-12-01 16:08:14 +0530

    update app.js

--- music/src/App.js
+++ music/src/App.js
@@ -1,58 +1,9 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Log from'./body/Log';
-// import Header from './Header';
 
 
 function App() {
   return (
-   <Router>
-     <div>
-     <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Log/>
-          </Route>
-        </Switch>
-      </div>
-   </Router>
+ <h1>React application</h1>
   );
-}
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 export default App;
