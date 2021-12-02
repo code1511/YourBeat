59553021b45a7e54b5f45d77a83f27a68ae46b4e
code1511 <anch20001511@gmail.com>
2021-12-02 15:37:12 +0530

    u

--- music/src/App.js
+++ music/src/App.js
@@ -1,28 +1,9 @@
-import React from 'react';
-const extractData = require('./extractData');
-const passReactState = require('./passReactState');
-
-//Recursively create all nested React components with reactData
-//Child elements are sent back back to extractData and then mapped over to be called recursively
-//TextContent is always passed as a child, if null react ignores it
-function makeChildNodes(reactData, stateData, getState) {
-
-  return reactData.map((obj, i) => {
-
-    return obj.children.length === 0
-
-      ? (
-          React.createElement(
-            obj.tag,
-            passReactState(obj, stateData, getState),
-            obj.props.textContent)
-        )
-
-      : React.createElement(obj.tag, obj.props, extractData(obj.children).mappedData.map(obj => makeChildNodes([obj], stateData, getState)))
-
-  });
-
+import React from "react"
+function App() {
+  return (
+   <h1>Hello</h1>
+  );
 }
 
-module.exports = makeChildNodes
+export default App;
 
