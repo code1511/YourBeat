342de23f383f8a1bb29721e346e841a8bb8530e3
code1511 <anch20001511@gmail.com>
2021-12-02 11:37:21 +0530

    u

--- music/src/App.js
+++ music/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-// import node from './diagram';
-// import rd3 from 'react-d3-library';
-// const RD3Component = rd3.Component;
+const extractData = require('./extractData');
+const passReactState = require('./passReactState');
 
-function App(){
-  return (
-    <h1>Hello Anu</h1>
-  );
+//Recursively create all nested React components with reactData
+//Child elements are sent back back to extractData and then mapped over to be called recursively
+//TextContent is always passed as a child, if null react ignores it
+function makeChildNodes(reactData, stateData, getState) {
+
+  return reactData.map((obj, i) => {
+
+    return obj.children.length === 0
+
+      ? (
+          React.createElement(
+            obj.tag,
+            passReactState(obj, stateData, getState),
+            obj.props.textContent)
+        )
+
+      : React.createElement(obj.tag, obj.props, extractData(obj.children).mappedData.map(obj => makeChildNodes([obj], stateData, getState)))
+
+  });
+
 }
-export default App;
+
+module.exports = makeChildNodes
+
