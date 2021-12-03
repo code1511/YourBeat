a2741ee8bef73950cc20a2ae253b06af8c326dc4
code1511 <anch20001511@gmail.com>
2021-12-03 16:28:36 +0530

    update

--- music/src/App.js
+++ music/src/App.js
@@ -8,77 +8,11 @@ import { LineChart,Line,
 
 const App = () => {
 //sample values to plot the chart
-const data = [
 
-  {name:'A', x:461},
-
-  {name:'B', x:862},
-
-  {name:'C', x:343},
-
-  {name:'D', x:454},
-
-  {name:'E', x:435},
-
-  {name:'F', x:653},
-
-  {name:'G', x:734},
-
-  {name:'H', x:845},
-
-  {name:'I', x:932},
-
-  {name:'J', x:133},
-
-  {name:'K', x:222},
-
-  {name:'L', x:332},
-
-  {name:'M', x:554},
-
-  {name:'N', x:554},
-
-  {name:'O', x:633},
-
-  {name:'P', x:722},
-
-  {name:'Q', x:638},
-
-  {name:'R', x:229},
-
-  {name:'S', x:321},
-
-  {name:'T', x:222},
-
-  {name:'U', x:573},
-
-  {name:'V', x:464},
-
-  {name:'W', x:565},
-
-  {name:'X', x:656},
-
-  {name:'Y', x:764},
-
-  {name:'Z', x:348},
-];
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},{name: 'Page A', uv: 400, pv: 2400, amt: 2400},{name: 'Page B', uv: 410, pv: 2100, amt: 2400}];
 
 
 return (
-
-  // <BarChart width={700} height={600} data={data} >
-
-  //     <CartesianGrid/>
-
-  //     <XAxis dataKey="name" />
-
-  //     <YAxis />
-
-  //     <Brush dataKey="name" height={30} stroke="#8084d8" />
-
-  //     <Bar dataKey="x" fill="red" />
-
-  // </BarChart>
 
   <LineChart width={500} height={300} data={data}>
     <XAxis dataKey="name"/>
