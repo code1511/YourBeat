c460dff80b530c076da0283031100cb7c8f2368c
code1511 <anch20001511@gmail.com>
2021-12-03 16:12:24 +0530

    rechart js

--- music/src/App.js
+++ music/src/App.js
@@ -1,71 +1,70 @@
-import React from 'react'; 
-import { BarChart, Bar, Brush, XAxis,  
+import React from 'react';
+import { BarChart, Bar, Brush, XAxis,
 
-    YAxis, CartesianGrid} from 'recharts'; 
+    YAxis, CartesianGrid} from 'recharts';
 
 
-const App = () => { 
+const App = () => {
+//sample values to plot the chart
+const data = [
 
-// Sample data 
-const data = [ 
+  {name:'A', x:861},
 
-  {name:'A', x:861}, 
+  {name:'B', x:862},
 
-  {name:'B', x:862}, 
+  {name:'C', x:343},
 
-  {name:'C', x:343}, 
+  {name:'D', x:454},
 
-  {name:'D', x:454}, 
+  {name:'E', x:435},
 
-  {name:'E', x:435}, 
+  {name:'F', x:653},
 
-  {name:'F', x:653}, 
+  {name:'G', x:734},
 
-  {name:'G', x:734}, 
+  {name:'H', x:845},
 
-  {name:'H', x:845}, 
+  {name:'I', x:932},
 
-  {name:'I', x:932}, 
+  {name:'J', x:133},
 
-  {name:'J', x:133}, 
+  {name:'K', x:222},
 
-  {name:'K', x:222}, 
+  {name:'L', x:332},
 
-  {name:'L', x:332}, 
+  {name:'M', x:554},
 
-  {name:'M', x:554}, 
+  {name:'N', x:554},
 
-  {name:'N', x:554}, 
+  {name:'O', x:633},
 
-  {name:'O', x:633}, 
+  {name:'P', x:722},
 
-  {name:'P', x:722}, 
+  {name:'Q', x:638},
 
-  {name:'Q', x:638}, 
+  {name:'R', x:229},
 
-  {name:'R', x:229}, 
+  {name:'S', x:321},
 
-  {name:'S', x:321}, 
+  {name:'T', x:222},
 
-  {name:'T', x:222}, 
+  {name:'U', x:573},
 
-  {name:'U', x:573}, 
+  {name:'V', x:464},
 
-  {name:'V', x:464}, 
+  {name:'W', x:565},
 
-  {name:'W', x:565}, 
+  {name:'X', x:656},
 
-  {name:'X', x:656}, 
+  {name:'Y', x:764},
 
-  {name:'Y', x:764}, 
-
-  {name:'Z', x:348}, 
+  {name:'Z', x:348},
 ];
 
 
 return (
 
-  <BarChart width={500} height={700} data={data} >
+  <BarChart width={500} height={600} data={data} >
 
       <CartesianGrid/>
 
