917c219beecc61b91346e06e80d944da7195e671
code1511 <anch20001511@gmail.com>
2021-12-04 21:21:17 +0530

    update

--- music/src/App.js
+++ music/src/App.js
@@ -9,20 +9,27 @@ import { LineChart,Line,
 const App = () => {
 //sample values to plot the chart
 
-const data = [{name: 'Page A', uv: 300, pv: 2490, amt: 400},
-              {name: 'Page B', uv: 800, pv: 1810, amt: 1100},
-              {name: 'Page C', uv: 410, pv: 2500, amt: 2400},
-              {name: 'Page A', uv: 300, pv: 2490, amt: 400},
-              {name: 'Page B', uv: 800, pv: 1810, amt: 1100},
-              {name: 'Page C', uv: 410, pv: 2500, amt: 2400}
+const data = [{name: 'SC1', uv: 300, pv: 2490, amt: 400},
+              {name: 'SC2', uv: 800, pv: 1810, amt: 1100},
+              {name: 'SC3', uv: 410, pv: 2500, amt: 2400},
+              {name: 'SC4', uv: 500, pv: 2390, amt: 400},
+              {name: 'SC5', uv: 2500, pv: 1810, amt: 1100},
+              {name: 'SC6', uv: 410, pv: 700, amt: 2400},
+              {name: 'SC7', uv: 2300, pv: 490, amt: 400},
+              {name: 'SC8', uv: 800, pv: 1810, amt: 1100},
+              {name: 'SC9', uv: 410, pv: 700, amt: 2400},
+              {name: 'SC10', uv: 300, pv: 2490, amt: 400},
+              {name: 'SC11', uv: 800, pv: 1810, amt: 1100},
+              {name: 'SC12', uv: 410, pv: 2500, amt: 2400},
 
             ];
 
 
 return (
 
-  <LineChart width={500} height={300} data={data}>
-    <XAxis />
+  <LineChart width={800} height={500} data={data}
+  margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <XAxis dataKey="name"/>
     <YAxis />
     <CartesianGrid stroke="#eee" strokeDasharray="9 9"/>
     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
