2e6859da06013372727af5f2fc64152e639539b3
code1511 <anch20001511@gmail.com>
2021-12-06 22:31:45 +0530

    update

--- music/src/App.js
+++ music/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { LineChart,Line,
-  //  BarChart, Bar, Brush,
+import { Area,Tooltip, AreaChart,
+  // LineChart,Line,
+    // BarChart, Bar, Brush,
     XAxis,
 
     YAxis, CartesianGrid} from 'recharts';
@@ -9,32 +10,93 @@ import { LineChart,Line,
 const App = () => {
 //sample values to plot the chart
 
-const data = [{name: 'SC1', uv: 300, pv: 2490, amt: 400},
-              {name: 'SC2', uv: 800, pv: 1810, amt: 1100},
-              {name: 'SC3', uv: 410, pv: 2500, amt: 2400},
-              {name: 'SC4', uv: 500, pv: 2390, amt: 400},
-              {name: 'SC5', uv: 2500, pv: 1810, amt: 1100},
-              {name: 'SC6', uv: 410, pv: 700, amt: 2400},
-              {name: 'SC7', uv: 2300, pv: 490, amt: 400},
-              {name: 'SC8', uv: 800, pv: 1810, amt: 1100},
-              {name: 'SC9', uv: 410, pv: 700, amt: 2400},
-              {name: 'SC10', uv: 300, pv: 2490, amt: 400},
-              {name: 'SC11', uv: 800, pv: 1810, amt: 1100},
-              {name: 'SC12', uv: 410, pv: 2500, amt: 2400},
+const data = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+]
 
-            ];
 
 
 return (
-
-  <LineChart width={500} height={300} data={data}
-  margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+<div>
+  {/* <div>
+  <LineChart width={500} height={300} data={data}>
     <XAxis dataKey="name"/>
     <YAxis />
     <CartesianGrid stroke="#eee" strokeDasharray="9 9"/>
     <Line type="monotone" dataKey="pv" stroke="#8884d8" />
     <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
   </LineChart>
+  </div>
+  <div>
+  <LineChart width={500} height={300} data={data}>
+    <XAxis dataKey="name"/>
+    <YAxis />
+    <CartesianGrid stroke="#eee" strokeDasharray="9 9"/>
+    <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+  </LineChart>
+  </div> */}
+  <AreaChart width={730} height={250} data={data}
+  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  <defs>
+    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+    </linearGradient>
+    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
+  <XAxis dataKey="name" />
+  <YAxis />
+  <CartesianGrid strokeDasharray="3 3" />
+  <Tooltip />
+  <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+  <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+</AreaChart>
+  </div>
 );
 }
 
