fce16fcc33a966e55ea1f65b25da7b4ac41772b7
code1511 <anch20001511@gmail.com>
2021-12-06 23:36:13 +0530

    added components

--- music/src/App.js
+++ music/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import {AreaGraph} from "./Components/AreaGraph";
+import {BarGraph} from "./Components/BarGraph";
+import {LineGraph} from "./Components/LineGraph";
+import {Header} from "./Header/Header"
 
 const App = () => {
-  return <div></div>;
+  return <div>
+    <Header/>
+  </div>;
 };
 
 export default App;
---
+++ music/src/Header/Header.jsx
@@ -0,0 +1,8 @@
+import "./Header.css";
+export default function Header(){
+    return(
+        <div>
+            Application name
+        </div>
+    );
+}
