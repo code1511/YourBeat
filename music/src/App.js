c74195d3bb17316ba7d7a8fb63b3a68be8743827
code1511 <anch20001511@gmail.com>
2021-12-06 22:40:47 +0530

    update

--- music/src/App.js
+++ music/src/App.js
@@ -12,43 +12,43 @@ const App = () => {
 
 const data = [
   {
-    "name": "Page A",
+    "name": "Jan",
     "uv": 4000,
     "pv": 2400,
     "amt": 2400
   },
   {
-    "name": "Page B",
+    "name": "Feb",
     "uv": 3000,
     "pv": 1398,
     "amt": 2210
   },
   {
-    "name": "Page C",
+    "name": "March",
     "uv": 2000,
     "pv": 9800,
     "amt": 2290
   },
   {
-    "name": "Page D",
+    "name": "April",
     "uv": 2780,
     "pv": 3908,
     "amt": 2000
   },
   {
-    "name": "Page E",
+    "name": "May",
     "uv": 1890,
     "pv": 4800,
     "amt": 2181
   },
   {
-    "name": "Page F",
+    "name": "June",
     "uv": 2390,
     "pv": 3800,
     "amt": 2500
   },
   {
-    "name": "Page G",
+    "name": "July",
     "uv": 3490,
     "pv": 4300,
     "amt": 2100
@@ -78,7 +78,7 @@ return (
   </LineChart>
   </div> */}
   <AreaChart width={730} height={250} data={data}
-  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  margin={{ top: 10, right: 30, left: 100, bottom: 0 }}>
   <defs>
     <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
       <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
