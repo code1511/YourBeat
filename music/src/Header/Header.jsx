cd9c6206e8194b865d8bf36630e97f3c03abdbb6
code1511 <anch20001511@gmail.com>
2021-12-07 13:46:49 +0530

    update

--- music/src/Header/Header.jsx
+++ music/src/Header/Header.jsx
@@ -3,8 +3,15 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header_d">
-      <div>
+      <div className="logo_l">
         <h1>App Name</h1>
+      </div>
+      <div className="right">
+        <div className="itemcontainer"></div>
+        <div className="itemcontainer"></div>
+        <div className="itemcontainer"></div>
+
+        <div className="itemcontainer"></div>
       </div>
     </div>
   );
