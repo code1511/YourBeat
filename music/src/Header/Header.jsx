3ee4e081f46befd07e81fbc40d100ff210671b7e
code1511 <anch20001511@gmail.com>
2021-12-07 13:57:05 +0530

    update

--- music/src/Header/Header.jsx
+++ music/src/Header/Header.jsx
@@ -1,18 +1,5 @@
 import React from "react";
 import "./Header.css";
 export default function Header() {
-  return (
-    <div className="header_d">
-      <div className="logo_l">
-        <h1>App Name</h1>
-      </div>
-      <div className="right">
-        <div className="itemcontainer">Home</div>
-        <div className="itemcontainer">Page A</div>
-        <div className="itemcontainer">Page B</div>
-
-        <div className="itemcontainer">Page C</div>
-      </div>
-    </div>
-  );
+  return <div className="header_d">App Name</div>;
 }
