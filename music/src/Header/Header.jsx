b96e609bbf91c25dbb09dad582f61124cbdbb901
code1511 <anch20001511@gmail.com>
2021-12-26 18:43:02 +0530

    update

--- music/src/Header/Header.jsx
+++ music/src/Header/Header.jsx
@@ -1,5 +1,48 @@
 import React from "react";
-import "./Header.scss";
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <div className="header_d">App Name</div>;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        {/* <Link to="/" style={{ textDecoration: "none" }}> */}
+          <span className="logo">Yourbeats </span>
+        {/* </Link> */}
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <PersonIcon />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <ChatIcon />
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <NotificationsIcon />
+                        <span className="topbarIconBadge">3</span>
+                    </div>
+                </div>
+            </div>
+    </div>
+  );
 }
