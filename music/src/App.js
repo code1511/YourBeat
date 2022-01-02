c5589dba62880c633927ae8b7b35b26c1e728358
code1511 <anch20001511@gmail.com>
2022-01-02 15:10:24 +0530

    update

--- music/src/App.js
+++ music/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import Intro from "./home/Intro";
-import Header from "./Header/Header";
 
 export default function App() {
   return (
     <div>
-      <Header />
       <Intro />
     </div>
   );
