ff3be0d579a1fb3937ead775368b783ebed2d7fa
code1511 <anch20001511@gmail.com>
2022-01-02 14:04:06 +0530

    update

--- music/src/Header/Header.jsx
+++ music/src/Header/Header.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import ChatIcon from '@mui/icons-material/Chat';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        {/* <Link to="/" style={{ textDecoration: "none" }}> */}
           <span className="logo">Yourbeats </span>
-        {/* </Link> */}
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
@@ -27,20 +21,6 @@ export default function Header() {
                 <div className="topbarLinks">
                     <span className="topbarLink">Homepage</span>
                     <span className="topbarLink">Timeline</span>
-                </div>
-                <div className="topbarIcons">
-                    <div className="topbarIconItem">
-                        <PersonIcon />
-                        <span className="topbarIconBadge">1</span>
-                    </div>
-                    <div className="topbarIconItem">
-                        <ChatIcon />
-                        <span className="topbarIconBadge">2</span>
-                    </div>
-                    <div className="topbarIconItem">
-                        <NotificationsIcon />
-                        <span className="topbarIconBadge">3</span>
-                    </div>
                 </div>
             </div>
     </div>
