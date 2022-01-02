cf25b6306dbf5499196f3469317f3cc33ff42978
code1511 <anch20001511@gmail.com>
2022-01-03 00:21:47 +0530

    update

--- music/src/home/Top/Top.js
+++ music/src/home/Top/Top.js
@@ -1,9 +1,26 @@
-export default function Top(){
-    return(
-        <div classNmae="Top_Container">
-            <div className="Logo_container">
-                <h1>Yourbeat</h1>
-            </div>
+import "./Top.css";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
+export default function Top() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">YourBeats</span>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Resume Reviewer</span>
+          <span className="topbarLink">Referrals</span>
+          <span className="topbarLink">Other Servies</span>
+          <span className="topbarLink">Testimonials
+          </span>
         </div>
-    )
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <AccountCircleSharpIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
