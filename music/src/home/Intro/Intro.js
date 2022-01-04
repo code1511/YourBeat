528281bb7c8b5bbf7e85d2fd9f4b935065bb17f6
code1511 <anch20001511@gmail.com>
2022-01-05 00:10:59 +0530

    update

--- music/src/home/Intro/Intro.js
+++ music/src/home/Intro/Intro.js
@@ -3,7 +3,7 @@ export default function Intro(){
     return(
         <div className="intro">
            <div className="resHeading">
-               <h1>Review your resume to get more opportunities </h1>
+               <h1>Review your resume to get more opportunities .</h1>
            </div>
             <input type="file"></input>
         </div>
