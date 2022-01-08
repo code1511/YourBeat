09c3418c584e5cd462e397ca4276847f7c37e7e3
code1511 <anch20001511@gmail.com>
2022-01-09 01:19:19 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
-const http =require("http");
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const path = require("path");
 
-http.createServer((req,res)=>{
+const { MongoClient } = require('mongodb');
 
-  res.writeHead(200,{'Content-type':'text/html'});
-  res.end('<h1>Hello world</h1>');
+const password = "<Anchal>";
+const uri = "mongodb+srv://code1511:Anchal@minhazdb.i0w7l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-}).listen(3000,()=>{
-    console.log("server running");
-})
+MongoClient.connect(uri, function(err, db) {
+  if (err) throw err;
+  // var dbo = db.db("mydb");
+  // var obj ={name:"Ankur dubey",address:"varanasi"};
+  // dbo.collection("customers").insertOne(obj,(err,res)=>{
+  //   if(err) throw err;
+  //   console.log("one value inserted");
+  //   db.close();
+  // })
+  console.log("db is working fine");
+  db.close();
+});
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
